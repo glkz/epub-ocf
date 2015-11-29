140f2c05cb1ffe985f7216edb2d7ba664c0f39fb
Mustafa Altun <gmail@mustafaaltun.com>
2015-11-29 22:01:44 +0200

    Update tests

--- test/ocf.test.js
+++ test/ocf.test.js
@@ -2,47 +2,53 @@ var ocf = require('../');
 var should = require('should');
 
 describe('#ocf', function() {
-  it('should return Zip container', function() {
-    ocf(__dirname + '/fixtures/sample.epub').should.be.instanceof(require('../lib/Zip'));
+  it('should return Zip container', function(done) {
+    ocf(__dirname + '/fixtures/sample.epub', {}, function(err, cont) {
+      cont.should.be.instanceof(require('../lib/zip').ZipContainer);
+      done();
+    });
   });
 });
 
 describe('#open', function() {
-  it('should return Zip async', function(done) {
+  it('should return Zip', function(done) {
     ocf.open(__dirname + '/fixtures/sample.epub', function(err, container) {
-      container.should.be.instanceof(require('../lib/Zip'));
+      container.should.be.instanceof(require('../lib/zip').ZipContainer);
       done();
     });
   });
 
-  it('should return Directory async', function(done) {
+  it('should return Dir', function(done) {
     ocf.open(__dirname + '/fixtures/sample', function(err, container) {
-      container.should.be.instanceof(require('../lib/Directory'));
+      container.should.be.instanceof(require('../lib/dir').DirContainer);
       done();
     });
   });
 
-  it('should return Http async', function(done) {
+  it('should return Http', function(done) {
     ocf.open('http://my.ebooks.service.org/ebook2/', function(err, container) {
-      container.should.be.instanceof(require('../lib/Http'));
+      container.should.be.instanceof(require('../lib/http').HttpContainer);
       done();
     });
   });
 });
 
-describe('# Zip container tests', function() {
-  containerTests(ocf.zip(__dirname + '/fixtures/sample.epub'));
+ocf.zip(__dirname + '/fixtures/sample.epub', {}, function(err, container) {
+  containerTests('# Zip container tests', container);
 });
 
-describe('# Directory container tests', function() {
-  containerTests(ocf.dir(__dirname + '/fixtures/sample'));
+ocf.dir(__dirname + '/fixtures/sample', {}, function(err, container) {
+  containerTests('# Dir container tests', container);
 });
 
-describe('# Http container tests ', function() {
-  var server;
-  var port = 9981;
 
-  before(function(done) {
+var server;
+var port = 9981;
+
+(function() {
+  var container = new ocf.http.HttpContainer('http://localhost:' + port, {});
+
+  var beforeFn = function(done) {
     var nodeStatic = require('node-static');
     var fileServer = new nodeStatic.Server(__dirname + '/fixtures/sample');
 
@@ -56,52 +62,61 @@ describe('# Http container tests ', function() {
       done();
     });
     server.listen(port);
-  });
+  };
 
-  describe('http', function() {
-    containerTests(ocf.http('http://localhost:' + port));
-  });
-
-  after(function() {
+  var afterFn = function() {
     server.close();
-  });
-});
+  };
+
+  containerTests('# Http container tests', container, beforeFn, afterFn);
+})();
+
 
 //
-function containerTests(container) {
-  describe('#rootfiles', function() {
-    it('should return list of rootfiles', function(done) {
-      container.rootfiles(function(err, files) {
-        files.should.be.an.Array;
-        files.should.containEql('EPUB/package.opf');
-        done();
+function containerTests(title, container, beforeFn, afterFn) {
+  describe(title, function() {
+    if (beforeFn) {
+      before(beforeFn);
+    }
+
+    describe('#rootfiles', function() {
+      it('should return list of rootfiles', function(done) {
+        container.rootfiles(function(err, files) {
+          files.should.be.an.Array;
+          files.should.containEql('EPUB/package.opf');
+          done();
+        });
       });
     });
-  });
 
-  describe('#readEntry', function() {
-    it('should return xml', function(done) {
-      container.readEntry('/EPUB/package.opf', function(err, content) {
-        content.indexOf('<?xml version="1.0" encoding="utf-8" standalone="no"?>').should.be.eql(0);
-        done();
+    describe('#readEntry', function() {
+      it('should return xml', function(done) {
+        container.readEntry('/EPUB/package.opf', function(err, content) {
+          content.indexOf('<?xml version="1.0" encoding="utf-8" standalone="no"?>').should.be.eql(0);
+          done();
+        });
       });
     });
-  });
 
-  describe('#createReadStream', function() {
-    it('should return readable stream', function() {
-      var stream = container.createReadStream('META-INF/container.xml');
-      stream.should.be.instanceof(require('stream').Readable);
-    });
+    describe('#createReadStream', function() {
+      it('should return readable stream', function() {
+        var stream = container.createReadStream('META-INF/container.xml');
+        stream.should.be.instanceof(require('stream').Readable);
+      });
 
-    it('listen data event', function(done) {
-      var stream = container.createReadStream('META-INF/container.xml');
-      stream.should.be.instanceof(require('stream').Readable);
+      it('listen data event', function(done) {
+        var stream = container.createReadStream('META-INF/container.xml');
+        stream.should.be.instanceof(require('stream').Readable);
 
-      stream.on('data', function(data) {
-        data.toString().indexOf('<?xml version="1.0"').should.be.eql(0);
-        done();
+        stream.on('data', function(data) {
+          data.toString().indexOf('<?xml version="1.0"').should.be.eql(0);
+          done();
+        });
       });
     });
+
+    if (afterFn) {
+      after(afterFn);
+    }
   });
 }
